0b93c313c14311e919c2c09de78cbb9d17115866
Umar Sohail <umarsohail648@gmail.com>
2025-09-22 21:52:08 +0500

    Delete backend directory

--- backend/middleware/errorHandler.js
+++
@@ -1,5 +0,0 @@
-module.exports = (err, req, res, next) => {
-  console.error('Unhandled error:', err);
-  if (res.headersSent) return next(err);
-  res.status(500).json({ message: 'Server Error', error: err.message || String(err) });
-};
